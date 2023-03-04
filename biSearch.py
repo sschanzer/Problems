45aeb918d223ea5f99035449c0a158306377574d
Spencer Schanzer <spencer.schanzer@gmail.com>
2023-03-04 13:32:07 -0800

    704. Binary Search

--- biSearch.py
+++ biSearch.py
@@ -11,11 +11,27 @@ You must write an algorithm with O(log n) runtime complexity.
 
 def biSearch(nums: list, target: int):
 
-    for i in nums:
-        if i == target:
-            return nums.index(i)
+    # for i in nums:
+    #     if i == target:
+    #         return nums.index(i)
     
-    return -1
+    # return -1
+
+    # Better Soultion:
+
+    l = 0 
+    r = len(nums) - 1
+    mid = (l + r) // 2
+
+    while l <= mid and mid < r:
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            l = mid + 1
+        elif nums[mid] > target:
+            r = mid - 1
+        mid = (l + r) // 2
+    return -1  
 
 
 if __name__ == '__main__':
